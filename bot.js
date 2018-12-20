f5a200d7406e3515469205a0c9efb5820c2d420d
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-12-20 20:39:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,17 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const client = new Discord.Client();
+const ytdl = require("ytdl-core");
+const { Client, Util } = require('discord.js');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const queue = new Map();
+var prefix = "="
+
+
+
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Code By : ZorexHost`);
@@ -5,6 +19,74 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Code By : ZorexHost `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Lord Broadcast',"http://twitch.tv/S-F")
+client.user.setGame(`Lord Broadcast`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 }); 
+
+
+
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
+
+client.on("message", message => {
+
+    if (message.content.startsWith(prefix + "obc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+m.send(`${argresult}\n ${m}`);
+})
+message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+message.delete(); 
+};     
+});
+
+
+client.on('message', message => {
+    var prefix = "=";
+   
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'ebc') {
+        if (!args[1]) {
+    message.channel.send("=ebc <message>");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+    });
